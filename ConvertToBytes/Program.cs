445ee699d7f21f28834f316e2d86dd4a59e1a563
rob.brz <robert.brzozowski@ftsgps.com>
2022-01-20 14:47:00 +0100

    Add project files.

---
+++ ConvertToBytes/Program.cs
@@ -0,0 +1,49 @@
+﻿using MatthiWare.CommandLine;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ConvertToBytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new CommandLineParserOptions
+            {
+                AppName = "CLI",
+            };
+            var parser = new CommandLineParser<ProgramOptions>();
+            var result = parser.Parse(args);
+            if (result.HasErrors)
+            {
+                Console.WriteLine("Parsing has error");
+                return;
+            }
+            var programOptions = result.Result;
+
+
+            byte[] input = File.ReadAllBytes(programOptions.Input);
+            string output = Convert.ToHexString(input);
+
+            var path = Directory.GetCurrentDirectory();
+            var fileName = programOptions.Output;
+            var fileLocation = Path.Combine(path, fileName);
+
+            string outputinInts = string.Empty;
+            Console.WriteLine(input.Length);
+            foreach (var b in input)
+            {
+                int intValue = Convert.ToInt32(b);
+                outputinInts += intValue+"-";
+            }
+
+            using (StreamWriter sw = File.CreateText(fileLocation))
+            {
+                sw.WriteLine(output);
+                sw.WriteLine(outputinInts);
+            }
+            Console.WriteLine($"File location is {fileLocation}");
+        }
+    }
+}
---
+++ ConvertToBytes/ProgramOptions.cs
@@ -0,0 +1,18 @@
+﻿using MatthiWare.CommandLine.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToBytes
+{
+    public class ProgramOptions
+    {
+        [Required, Name("file","i" ), Description("file will be converted to hex")]
+        public string Input { get; set; }
+
+        [Required, Name("outputfile", "o"), Description("file with result of conversion")]
+        public string Output { get; set; }
+    }
+}
